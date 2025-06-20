733bd0e8488ed76ff0969ab20c0f4348bcdf1e6c
Erwin Gunawan <erwin.dg8ts@gmail.com>
2025-06-20 22:35:05 +0700

    dashboard grid

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,5 +1,9 @@
 export default function AppFooter() {
     return (
-        <>App Footer</>
+        <footer className="border-t mt-auto border-black/5 py-5">
+            <small className="opacity-50">
+                &copy; 2030 Erwin. All rights reserved.
+            </small>
+        </footer>
     )
 }
